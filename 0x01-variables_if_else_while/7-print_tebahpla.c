2726f9de85f63a03f7626d4107abb5ba8f8f3552
yvonne <vnnmukami@gmail.com>
2023-02-20 14:24:09 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char t;
+
+	t = 'z';
+
+	while (t <= 'z')
+	{
+		putchar(t);
+		t--;
+	}
+	putchar('\n');
+	return (0);
+}
