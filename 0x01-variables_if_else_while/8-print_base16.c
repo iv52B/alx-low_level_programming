3b79dcb87b166d1daa6c234b40129cbd0fd37c66
yvonne <vnnmukami@gmail.com>
2023-02-20 14:44:24 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char d;
+
+	int h;
+
+	d = 'a'';
+	while (h < 16)
+	{
+		putchar('h + '0');
+		h++;
+	}
+	while (d <= 'f')
+	{
+		putchar(d);
+		d++;
+	}
+	return (0);
+}
